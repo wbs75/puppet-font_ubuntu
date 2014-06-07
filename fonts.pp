95fa92f95add9a90e8eed01219994cb078d60852
William Smith <wbs@mavericks.(none)>
2014-06-07 09:58:49 -0600

    added fonts.pp

---
+++ fonts.pp
@@ -0,0 +1,25 @@
+class projects::fonts (
+    $my_homedir   = $people::wbs75::params::my_homedir,
+    $my_sourcedir = $people::wbs75::params::my_sourcedir,
+    $my_username  = $people::wbs75::params::my_username
+    ){
+
+    File {
+        owner => $my_username,
+        group => 'admin',
+        mode  => '0644',
+    }
+
+    boxen::project { 'boxen-desktop':
+        dir     => "${my_homedir}/src/font_ubuntu",
+        source  => 'https://github.com/wbs75/puppet-font_ubuntu',
+    }
+
+
+    file {"${my_homedir}/Library/Fonts":
+      ensure => 'directory',
+      recurse => true,
+      source => "${my_homedir}/src/font_ubuntu"
+    }
+
+}

d62f9e1228e1c5dc744a26fb59ca70836fe845df
William Smith <wbs@mavericks.(none)>
2014-06-07 16:37:57 -0600

    removed mode

--- font_ubuntu.pp
+++ font_ubuntu.pp
@@ -4,21 +4,14 @@ class projects::font_ubuntu (
     $my_username  = $people::wbs75::params::my_username
     ){
 
-    File {
-        owner => 'root',
-        group => 'wheel',
-        mode  => '0644',
-    }
-
     boxen::project { 'font_ubuntu':
         dir     => "${my_homedir}/src/puppet-font_ubuntu",
         source  => 'https://github.com/wbs75/puppet-font_ubuntu',
     }
 
 
-    file {"/Library/Fonts":
+    file {"/Library/Fonts/":
       ensure =>     'directory',
-      mode  =>      '0644',
       recurse =>    true,
       source =>     "${my_homedir}/src/puppet-font_ubuntu/ubuntu_font_family"
     }
